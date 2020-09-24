23f4b49c1b2491cd1819af7325900e7a25478076
Henrique Laki <henrique.laki@gmail.com>
2020-09-24 01:12:01 -0300

    Initial Commit

---
+++ docker_postgres_init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE receitas
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'pt_BR.utf8'
+    LC_CTYPE = 'pt_BR.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
